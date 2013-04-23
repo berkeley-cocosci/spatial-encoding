0ca4c85834bb19018ce5916fcc3bbd0305488286
Crystal Chen <crystalus95@gmail.com>
2013-04-23 01:55:16 -0700

    Redraft of code
    New design and updates detailed in Update 4/22/2013 section of plans.

--- spatial_learning.py
+++ spatial_learning.py
@@ -1,77 +1,107 @@
-""" PLANS
+import math
 
--dictionary of items/animals name strings -> integer representations?
+"""
+spatial_learning.py
+
 
 """
 
-class Constraint(object):
+	def __init__(self, obj="X", x=0, y=0):
+		self.total_rooms = num #total number of rooms
 
-<<<<<<< HEAD
-  def __init__(self, piv, pos, item):
-		self.pivot = piv
-		self.position = pos
-=======
-	def __init__(self, piv, rel, item):
-		self.pivot = piv
-		self.relation = rel
->>>>>>> Transfer local spatial_learning.py file to repo
-		self.item = item
+		self.item = obj #item stored in this Room
+		# self.x_coord = x
+		# self.y_coord = y
 
-	def __init__(self, msg):
-		self.info = interpret(msg)
-		self.pivot = info[0]
-		self.position = info[1]
-		self.item = info[3]
+		self.left = [] #list of rooms to the left of this
+		self.right = [] #list of rooms to the right of this
+		self.top = [] #list of rooms above this
+		self.bottom = [] #list of rooms below this
 
-	#parse the user input and returns a list [pivot, position, item]
-	def interpret(msg):
-		return
+		self.locations = Grid(total_rooms) #2D list of possible locations for this Room
+
+	# def set_location(x_coord, y_coord):
+	# 	"""set this Room to location (x, y) in the grid"""
+	# 	x_coord = x
+	# 	y_coord = y
+	# 	locations.add_to(this, x, y)
+
+	def set_left_of(room):
+		"""set this Room left of parameter Room x"""
+		right.append(room)
+		locations.ignore_column(total_rooms-1) #last column becomes invalid for this
+		room.locations.ignore_column(0) #first column becomes invalid for room
+
+	def set_right_of(room):
+		"""set this Room right of parameter Room x"""
+		left.append(room)
+		locations.ignore_column(0) #first column becomes invalid for this
+		room.locations.ignore_column(total_rooms-1) #last column becomes invalid for room
+
+	def set_above(room):
+		"""set this Room above parameter Room x"""
+		bottom.append(room)
+		locations.ignore_row(total_rooms-1) #last row becomes invalid for this
+		room.locations.ignore_row(0) #first row becomes invalid for room
+
+	def set_below(room):
+		"""set this Room below parameter Room x"""
+		top.append(room)
+		locations.ignore_row(0) #first row becomes invalid for this
+		room.locations.ignore_row(total_rooms-1) #last row becomes invalid for room
 
 
-class Layout(object):
 
-	def __init__(self, x=0, y=0):
-		"""Create a Layout with the given dimensions."""
-		self.width = x
-		self.height = y
-		self.grid = [[]] #A 2D list
-		self.constraints = []
+class Grid(object):
 
-	#Adds the constraint to this layout and updates it accordingly
-	def add_constraint(rule):
-		constraints.append(rule)
-		#update the layout
+	def __init__(self, num=0):
+		"""Create a Layout base grid by the given number of items."""
+		self.size = num
+		self.locs = [[]] #a 2D list
 
-	#Prints out the string representation of each possible layout
+	def add_to(room, x, y):
+		"""Adds the given room to location (x,y) in the grid"""
+		locs[x][y] = room
+		room.set_location(x, y)
+
+	def ignore_column(x):
+		"""Sets the column x to Rooms with item 'X', representing an invalid location"""
+		for i in range(size-1):
+			locs[x][i].item = "X"
+
+	def ignore_row(y):
+		"""Sets the row y to Rooms with item 'X', representing an invalid location"""
+		for i in range(size-1):
+			locs[i][y].item = "X"
+
 	def toString():
-		for x in range(width):
-			for y in range(height):
-				print(grid[x][y]+" ")
+		"""Prints the string representation of this grid"""
+		output = ""
+		for x in range(size):
+			for y in range(size):
+				if (locs[x][y] == null):
+					output = " "
+				else:
+					output = locs[x][y].item
+				print(output+" ")
 			print("\n")
 
 
-class Experiment(object):
+
+class Experiment(object, n):
 
 	def __init__(self):
-		self.results = [] #list of all possible layouts
-
-	#Generates the valid layouts up to this point
-	#for each of the existing possible layouts, add and update with new constraints
-	#HOW TO HANDLE IF FOR ONE LAYOUT THE NEW CONSTRAINT CAN HAVE MULTIPLE NEW LAYOUTS?!?!
-	def update_layouts(updates):
-		for info in updates:
-			msg = Constraint(info)
-			for layout in results:
-				layout.add_constraint(msg)
-
-<<<<<<< HEAD
-=======
+		self.results = [] #list of Grid obejcts representing all possible layouts
 
 
->>>>>>> Transfer local spatial_learning.py file to repo
-#To test my hand in python...it's been a while
+
+
+
+"""For practice with python and reading user input"""
 def main(*args):
+	test = Experiment()
 	input_var = raw_input("Enter constraint: ")
 	print("You typed: " + input_var)
 
 main()
+
