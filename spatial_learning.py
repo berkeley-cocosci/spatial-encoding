5a49d70dcc2772e61161e3c40750d61b6fe69f32
Crystal Chen <crystalus95@gmail.com>
2013-04-30 13:04:44 -0700

    Added tests and updated spatial-learning
    Combined scrap.py and spatial-learning.py
    Added more unit tests to tests.py

--- spatial_learning.py
+++ spatial_learning.py
@@ -3,105 +3,162 @@ spatial_learning.py
 
 
 """
-
+    
 class Room(object):
-	
-	def __init__(self, obj="X", x=0, y=0):
-		self.total_rooms = num #total number of rooms
+    """A Room has position (x, y) knows its relations to other Room objects"""
 
-		self.item = obj #item stored in this Room
-		# self.x_coord = x
-		# self.y_coord = y
+    def __init__(self, x, y):
+        """Constructs a Room with the given position (x, y)"""
+        self.x = x
+        self.y = y
 
-		self.left = [] #list of rooms to the left of this
-		self.right = [] #list of rooms to the right of this
-		self.top = [] #list of rooms above this
-		self.bottom = [] #list of rooms below this
+    def is_in_back_of(self, other):
+        """Returns true if self is in the back of other, false otherwise"""
+        return self.y > other.y
 
-		self.locations = Grid(total_rooms) #2D list of possible locations for this Room
+    def is_in_front_of(self, other):
+        """Returns true if self in in front of other, false otherwise"""
+        return other.in_back_of(self)
 
-	# def set_location(x_coord, y_coord):
-	# 	"""set this Room to location (x, y) in the grid"""
-	# 	x_coord = x
-	# 	y_coord = y
-	# 	locations.add_to(this, x, y)
+    def is_left_of(self, other):
+        """Returns true if self is left of other, false otherwise"""
+        return self.x < other.x
 
-	def set_left_of(room):
-		"""set this Room left of parameter Room x"""
-		right.append(room)
-		locations.ignore_column(total_rooms-1) #last column becomes invalid for this
-		room.locations.ignore_column(0) #first column becomes invalid for room
+    def is_right_of(self, other):
+        """Returns true if self is right of other, false otherwise"""
+        return other.is_left_of(self)
 
-	def set_right_of(room):
-		"""set this Room right of parameter Room x"""
-		left.append(room)
-		locations.ignore_column(0) #first column becomes invalid for this
-		room.locations.ignore_column(total_rooms-1) #last column becomes invalid for room
-
-	def set_above(room):
-		"""set this Room above parameter Room x"""
-		bottom.append(room)
-		locations.ignore_row(total_rooms-1) #last row becomes invalid for this
-		room.locations.ignore_row(0) #first row becomes invalid for room
-
-	def set_below(room):
-		"""set this Room below parameter Room x"""
-		top.append(room)
-		locations.ignore_row(0) #first row becomes invalid for this
-		room.locations.ignore_row(total_rooms-1) #last row becomes invalid for room
+    def to_string():
+        """Returns the string representation of this Room"""
+        return "Room at (%d, %d)" % (self.x, self.y)
 
 
 
-class Grid(object):
+class World(object):
+    """A World has animals and sets them to Rooms given constraints"""
 
-	def __init__(self, num=0):
-		"""Create a Layout base grid by the given number of items."""
-		self.size = num
-		self.locs = [[]] #a 2D list
+    def __init__(self, animals):
+        """Constructs a World with the given animals with Rooms initialized to None"""
+        self.animals = {}
+        for a in animals:
+            self.animals[a] = None
 
-	def add_to(room, x, y):
-		"""Adds the given room to location (x,y) in the grid"""
-		locs[x][y] = room
-		#room.set_location(x, y)
+    def set_position(self, animal, x, y):
+        """Sets the given animal to a new Room at position (x, y) and returns the room.
+            Raises a ValueError if the animal is already set."""
+        if self.animals[animal] is None:
+            room = Room(x, y)
+            self.animals[animal] = room
+            return room
+        else:
+            raise ValueError("Animal %s is already set at (%d, %d)" % (animal, self.animals[animal].x, self.animals[animal].y))
 
-	def ignore_column(x):
-		"""Sets the column x to Rooms with item 'X', representing an invalid location"""
-		for i in range(size-1):
-			locs[x][i].item = "X"
+    def test_constraint(self, constraint):
+        """Tests whether the given constraint is satisfied"""
+        animal1, relation, animal2 = constraint
+        if self.animals[animal1] is None:
+            return True
+        if self.animals[animal2] is None:
+            return True
+        if relation == "in front of":
+            return self.animals[animal1].is_in_front_of(self.animals[animal2])
+        elif relation == "in back of":
+            return self.animals[animal1].is_in_back_of(self.animals[animal2])
+        elif relation == "left of":
+            return self.animals[animal1].is_left_of(self.animals[animal2])
+        else:
+            return self.animals[animal1].is_right_of(self.animals[animal2])
 
-	def ignore_row(y):
-		"""Sets the row y to Rooms with item 'X', representing an invalid location"""
-		for i in range(size-1):
-			locs[i][y].item = "X"
+    def copy(self):
+        """Creates and returns a copy of this World"""
+        World new_world = World(self.animals)
+        return new_world
 
-	def toString():
-		"""Prints the string representation of this grid"""
-		output = ""
-		for x in range(size):
-			for y in range(size):
-				if (locs[x][y] == null):
-					output = " "
-				else:
-					output = locs[x][y].item
-				print(output+" ")
-			print("\n")
+    def to_string():
+        """Prints the string representation of this World"""
+        output = ""
+        for animal in self.animals:
+            output += animal+"\n"
+            for room in self.animals[animal]:
+                output += room.to_string()+"\n"
+                print(output+" ")
+            print("\n")
 
 
 
-class Experiment(object, n):
+class Experiment(object):
 
-	def __init__(self):
-		self.results = [] #list of Grid obejcts representing all possible layouts
+    def __init__(self, animals=[], constraints=[]):
+        """Initializes an experiment with all possible worlds given animals and constraints"""
+        self.animals = animals
+        self.constraints = constraints
+        self.possible_worlds = []
+        #Initialize the first animal (constraints trivial) to all positions
+        for x in xrange(len(animals)):
+            for y in xrange(len(animals)):
+                world = World(animals)
+                world.set_position(animals[0], x, y)
+                possible_worlds.append(world)
+        update()
+
+    def update():
+        """Updates the possible worlds for this Experiment"""
+        if len(animals) <= 1:
+            return None
+        for animal in animals[1:]:
+            new_possible_worlds = []
+            for world in possible_worlds:
+                new_worlds = []
+                for x in xrange(len(animals)):
+                    for y in xrange(len(animals)):
+                        new_world = world.copy()
+                        new_world.set_position(animal, x, y)
+                        valid = True
+
+                        for constraint in constraints:
+                            valid = new_world.test_constraint(constraint)
+                            if not valid:
+                                break
+
+                        if valid:
+                            new_worlds.append(new_world)
+
+                if len(new_worlds) > 0:
+                    new_possible_worlds.append(new_worlds)
+
+            possible_worlds = new_possible_worlds
 
 
+    def add_animal(animal):
+        """Adds the animal to the list of animals in this Experiment.
+           Does nothing if the animal already exists."""
+        if animal not in self.animals:
+            self.animals.append(animal)
 
+    def add_constraint(constraint):
+        """Adds the constraint to the list of constraints in this Experiment.
+           Adds animals included in the constraint as needed."""
+        if len(constraint) < 3:
+            raise ValueError("Invalid constraint format")
+        self.add_animal(constraint[0])
+        self.add_animal(constraint[1])
+        self.constraints.append(constraint)
 
+    def main(*args):
+        animals = ['cat', 'dog', 'fish']
 
-"""For practice with python and reading user input"""
-def main(*args):
-	test = Experiment()
-	input_var = raw_input("Enter constraint: ")
-	print("You typed: " + input_var)
+        # Possible relations: in front of, in back of, left of, right of
+        constraints = [
+            ('cat', 'in front of', 'dog'),
+            ('dog', 'left of', 'fish')
+        ]
 
-main()
+        experiment = Experiment(animals, constraints)
+        print("Possible relations are: in front of, in back of, left of, right of.")
+        while (animal != "done"):
+            animal = raw_input("Enter animal (or \"done\"): ")
+            experiment.add_animal(animal)
+        while (constraint != "done"):
+            constraint = raw_input("Enter constraint (or \"done\"): ")
+            experiment.add_constraint(constraint)
 
--- tests.py
+++ tests.py
@@ -1,6 +1,110 @@
-from spatial_learning import Room
+from spatial_learning import Room, World, Experiment
 
-def test_room_left_of():
+"""Tests for Room"""
+
+def test_room_init():
+    return None
+
+def test_room_relations():
+    print("Testing left_of")
+    test_room_left_of()
+    print("Testing right_of")
+    test_room_right_of()
+    print("Testing in_front_of")
+    test_room_in_front_of()
+    print("Testing in_back_of")
+    test_room_in_back_of()
+
+def test_is_left_of():
     r1 = Room(1, 1)
     r2 = Room(2, 2)
     assert r1.is_left_of(r2)
+    assert not r2.is_left_of(r1)
+
+def test_is_right_of():
+    r1 = Room(2, 2)
+    r2 = Room(1, 1)
+    assert r1.is_right_of(r2)
+    assert not r2.is_right_of(r1)
+
+def test_is_in_front_of():
+    r1 = Room(2, 2)
+    r2 = Room(1, 1)
+    assert r1.is_in_front_of(r2)
+    assert not r2.is_in_front_of(r1)
+
+def test_is_in_back_of():
+    r1 = Room(1, 1)
+    r2 = Room(2, 2)
+    assert r1.is_in_back_of(r2)
+    assert not r2.is_in_back_of(r1)
+
+
+
+"""Tests for World"""
+
+def test_world_init():
+    return None
+
+def test_set_position():
+    return None
+
+def test_test_constraint():
+    return None
+
+def test_copy():
+    w1 = World()
+    w2 = w1.copy()
+    assert w1 is w2 #not the same object
+    assert w1.toString() == w2.toString() #but the identical contents
+
+
+
+"""Tests for Experiment"""
+
+def test_experiment_init():
+    animals = ['dog', 'cat']
+    constraints = [('dog', 'left of', 'cat')]
+    exp = Experiment(animals, constraints)
+    assert exp.animals == animals
+    assert exp.constraints == constraints
+    
+
+def test_add_animal():
+    exp = Experiment()
+    exp.add_animal('dog')
+    exp.add_animal('cat')
+    assert exp.animals == ['dog', 'cat']
+
+def test_add_constraint():
+    c1 = ('dog', 'left of', 'cat')
+    c2 = ('dog', 'in front of', 'frog')
+    exp = Experiment(['dog', 'cat'])
+    exp.add_constraint(c1)
+    exp.add_constraint(c2)
+    assert exp.animals == ['dog', 'cat', 'frog']
+    assert exp.constraints == [c1, c2]
+
+def test_update():
+    return None
+
+
+
+"""Main testing method"""
+@main
+def main(*args):
+    print("Testing Room")
+    test_room_init()
+    test_room_relations()
+
+    print("Testing World")
+    test_world_init()
+    test_set_position()
+    test_test_constraint()
+    test_copy()
+
+    print("Testing Experiment")
+
+
+main()
+
